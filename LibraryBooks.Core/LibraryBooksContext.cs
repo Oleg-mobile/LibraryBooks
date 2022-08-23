2273e908fcffa5546243529fa96a7434dc787757
Oleg Ivanov <juklino@yandex.ru>
2022-08-23 13:10:19 +0300

    + добавлены модели таблиц БД + добалвена модель БД

---
+++ LibraryBooks.Core/LibraryBooksContext.cs
@@ -0,0 +1,23 @@
+﻿using LibraryBooks.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryBooks.Core
+{
+    public class LibraryBooksContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //
+        }
+    }
+}
---
+++ LibraryBooks.Core/Models/Book.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryBooks.Core.Models
+{
+    public class Book : Entity
+    {
+        public int UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+
+        public int AuthorId { get; set; }
+
+        [ForeignKey("AuthorId")]
+        public Author Author { get; set; }
+
+        public int GenreId { get; set; }
+
+        [ForeignKey("GenreId")]
+        public Genre Genre { get; set; }
+
+        public string Name { get; set; }
+        public string Publication { get; set; }
+        public int Year { get; set; }
+        public int Mark { get; set; }
+        public int PageCount { get; set; }
+        public string PathToBook { get; set; }
+        public bool IsLiked { get; set; }
+        public bool IsFinished { get; set; }
+    }
+}
