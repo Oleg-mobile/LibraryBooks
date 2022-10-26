2f3080f0a9a210c0a8bf745e456bcca2a97f4e39
Oleg Ivanov <juklino@yandex.ru>
2022-10-26 16:21:02 +0300

    + Добавил DTO Book + Добавил DTO Genre

---
+++ LibraryBooks/Dto/Authors/BookDto.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using LibraryBooks.Attributes;
+using LibraryBooks.Core.Models;
+
+namespace LibraryBooks.Dto.Authors
+{
+    [AutoMap(typeof(Book), ReverseMap = true)]
+    public class BookDto : EntityDto
+    {
+        [DgvColumn(DisplayName = "Пользователь", IsVisible = false)]
+        public int UserId { get; set; }
+        [DgvColumn(DisplayName = "Автор")]
+        public int AuthorId { get; set; }
+        [DgvColumn(DisplayName = "Жанр")]
+        public int GenreId { get; set; }
+        [DgvColumn(DisplayName = "Название")]
+        public string Name { get; set; }
+        [DgvColumn(DisplayName = "Издание", IsVisible = false)]
+        public string Publication { get; set; }
+        [DgvColumn(DisplayName = "Год выпуска", IsVisible = false)]
+        public int Year { get; set; }
+        [DgvColumn(DisplayName = "Закладка", IsVisible = false)]
+        public int Mark { get; set; }
+        [DgvColumn(DisplayName = "Количество страниц", IsVisible = false)]
+        public int PageCount { get; set; }
+        [DgvColumn(DisplayName = "Путь к книге")]
+        public string PathToBook { get; set; }
+        [DgvColumn(DisplayName = "Понравилась", IsVisible = false)]
+        public bool IsLiked { get; set; }
+        [DgvColumn(DisplayName = "Прочитана", IsVisible = false)]
+        public bool IsFinished { get; set; }
+    }
+}
---
+++ LibraryBooks/Dto/Authors/GenreDto.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using LibraryBooks.Attributes;
+using LibraryBooks.Core.Models;
+
+namespace LibraryBooks.Dto.Authors
+{
+    [AutoMap(typeof(Genre), ReverseMap = true)]
+    public class GenreDto : EntityDto
+    {
+        [DgvColumn(DisplayName = "Название жанра")]
+        public string Name { get; set; }
+    }
+}
