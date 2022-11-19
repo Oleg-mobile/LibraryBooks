4d82d661a1b55eb7114d6db92f744e3dfb8bb445
Oleg Ivanov <juklino@yandex.ru>
2022-11-19 11:25:34 +0300

    Revert "+ отказался переопределять метод InitializeComponent на больших формах (ключевое слово new)"
    This reverts commit c99317e0106f76d0993834981d5a4c0c65c7dab6.

--- LibraryBooks/Forms/FormGenres.Designer.cs
+++ LibraryBooks/Forms/FormGenres.Designer.cs
@@ -26,7 +26,7 @@
         /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
         /// </summary>
-        private new void InitializeComponent()
+        private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormGenres));
             this.dataGridViewGenres = new System.Windows.Forms.DataGridView();
